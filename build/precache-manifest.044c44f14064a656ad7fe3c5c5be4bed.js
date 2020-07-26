628777a7d3aeb67697cffc3c98f48a75325d3c01
Niklas Kröger <niklas.kroger@aalto.fi>
2020-07-26 15:24:03 +0300

    uibuild

---
+++ build/precache-manifest.044c44f14064a656ad7fe3c5c5be4bed.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f2da7887f2237e31828294cb3f4ce808",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c0a118d07881fa40e8c3",
+    "url": "/static/css/main.db30603f.chunk.css"
+  },
+  {
+    "revision": "edb70b9760340e495c07",
+    "url": "/static/js/2.42c16a5d.chunk.js"
+  },
+  {
+    "revision": "c0a118d07881fa40e8c3",
+    "url": "/static/js/main.6407070d.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
